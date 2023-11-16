7b7f81ae376bcf4dbef537bc4aae8c04edfb0b74
Thomas Heinlein <github@thomasheinlein.com>
2023-11-16 21:06:53 +0100

    clean up edit page

--- src/character/CharacterEdit.js
+++ src/character/CharacterEdit.js
@@ -4,8 +4,26 @@ import { Alert } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import yaml from "js-yaml";
 import isParsableCharacter from "./CharacterParsableChecker";
+import Button from "@mui/material/Button";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import DownloadIcon from "@mui/icons-material/Download";
+import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 
 const CharacterEdit = ({ character, setCharacter }) => {
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: "#f5f5f9",
+      color: "rgba(0, 0, 0, 0.87)",
+      maxWidth: 350,
+      minWidth: 150,
+      fontSize: theme.typography.pxToRem(12),
+      border: "1px solid #dadde9",
+    },
+  }));
+
   const [alert, setAlert] = useState(
     isParsableCharacter(character) ? "parsable" : "not_parsable",
   );
@@ -18,6 +36,17 @@ const CharacterEdit = ({ character, setCharacter }) => {
     } catch (e) {
       return false;
     }
+  };
+
+  const downloadTextFile = () => {
+    const fileContent = yaml.dump(character);
+    const blob = new Blob([fileContent], { type: "text/plain" });
+    const downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = "Character.yml";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   };
 
   const handleChange = (event) => {
@@ -37,21 +66,6 @@ const CharacterEdit = ({ character, setCharacter }) => {
 
   return (
     <Stack spacing={3}>
-      <Alert severity="info">
-        Here you can edit the character data, which is shown in the second tab.
-        If the format is valid, the changes will be automatically saved. Please
-        note that the data is stored only within your browser! If you change the
-        browser or clear the cookies you will loose your character stats. Please
-        create a backup by copying the text below into a file! You can restore
-        the backup by pasting the text back into the text field.
-      </Alert>
-      <Alert severity="info">
-        The text is shown in YAML format. Note that spaces and indent matter! If
-        you have problems you can always ask ChatGPT to reformat your character
-        values into a valid YAML format. Each Element in your character sheet
-        must contain of a title followed by items. Each item can be a value (for
-        a single item) or a description and a value (for a key value pair).
-      </Alert>
       {alert === "parsable" && (
         <Alert severity="success">Character sheet valid!</Alert>
       )}
@@ -59,6 +73,51 @@ const CharacterEdit = ({ character, setCharacter }) => {
         <Alert severity="error">Character sheet not valid!</Alert>
       )}
 
+      <Stack direction="row" justifyContent="space-between">
+        <Button
+          variant="contained"
+          startIcon={<DownloadIcon />}
+          onClick={downloadTextFile}
+        >
+          Download
+        </Button>
+        <HtmlTooltip
+          placement="left-start"
+          title={
+            <React.Fragment>
+              <p>{`Here you can edit the character data, which is shown in the second
+              tab. If the format is valid, the changes will be automatically
+              saved. Please note that the data is stored only within your
+              browser! If you change the browser or clear the cookies you will
+              loose your character stats. Please create a backup by clicking the download
+              button and store the file on your device. You can restore the backup by pasting 
+              the text back into the text field.`}</p>
+              <p>{`The text is shown in YAML format. Note that spaces and indent
+              matter! If you have problems you can always ask ChatGPT to
+              reformat your character values into a valid YAML format. Here is a 
+              valid example:`}</p>
+              <pre>{`
+- title: General
+  items:
+    - description: Hit Points
+      value: 20
+    - description: Armor Class
+      value: 15
+- title: Equipment
+  items:
+    - value: Leather Armor
+    - value: Leather Helmet
+`}</pre>
+              <p>{`Note that you can specify key value pairs (like Hit Points and 20) 
+              and simple list items (like Leather Helmet).`}</p>
+            </React.Fragment>
+          }
+        >
+          <Button variant="outlined" startIcon={<QuestionMarkIcon />}>
+            Help
+          </Button>
+        </HtmlTooltip>
+      </Stack>
       <TextField
         fullWidth
         error={alert === "not_parsable"}
